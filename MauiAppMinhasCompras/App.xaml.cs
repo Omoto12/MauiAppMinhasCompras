aecdb9322dbec9abbbfbce31a1fbfaa3cc81488e
Giovana Omoto <giovana.omoto@gmail.com>
2026-08-18 19:19:02 -0300

    agenda03

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -1,17 +1,29 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MauiAppMinhasCompras.Helpers;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace MauiAppMinhasCompras
 {
     public partial class App : Application
     {
-        public App()
+        static SQLiteDatabaseHelper _db; //Cria uma variável p/ guardar a referência do BDs;
+                                         //variável é compartilhada por todo o aplicativo.
+        public static SQLiteDatabaseHelper Db //É uma propriedade global que permite que outra tela do aplicativo
+                                              //acesse o banco de dados chamando o App.Dd
         {
-            InitializeComponent();
-        }
+            get
+            {
+                if(_db == null) //evita repetição de arquivos e conexões
+                {
+                 string path = Path.Combine(
+                 Environment.GetFolderPath(
+                 Environment.SpecialFolder.LocalApplicationData),
+                 "banco_sqlite_compras.db3");
 
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
+                 _db = new SQLiteDatabaseHelper(path);
+                }
+
+                return _db;
+            }
         }
     }
 }
--- MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
+++ MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -1,28 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using MauiAppMinhasCompras.Models;
+﻿using MauiAppMinhasCompras.Models;
 using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
 
 namespace MauiAppMinhasCompras.Helpers
 {
     public class SQLiteDatabaseHelper
     {
-        readonly SQLiteAsyncConnection _conn;
+        readonly SQLiteAsyncConnection _conn; //Conexão do banco de dados não seja substituída;Gerenciar operações com o banco de dados assíncrona
 
-        public SQLiteDatabaseHelper(string path)
+        public SQLiteDatabaseHelper(string path) //inicializa a conexão com o banco de dados e criada a tabela assim que a instância for criada
         {
             _conn = new SQLiteAsyncConnection(path);
 
-            _conn.CreateTableAsync<Produto>().Wait();
+            _conn.CreateTableAsync<Produto>().Wait(); // Cria tabela assíncrona Produto com base na classe modelo Produto. Se já existir não faz nada
         }
 
-        public Task<int> Insert(Produto p)
+        public Task<int> Insert(Produto p) //Task retorna um valor inteiro
         {
-            return _conn.InsertAsync(p);
+            return _conn.InsertAsync(p); //Passa o objeto produto para o banco de dados
+
         }
 
-        public Task<List<Produto>> Update(Produto p)
+        public Task<List<Produto>> Update(Produto p) //Método Update, atualiza um registro existente na tabela Produto no BDs, com o ID fornecido no objeto Produto
         {
             string sql = "UPDATE Produto SET Descricao=?, Quantidade=?, Preco=? WHERE Id=?";
             return _conn.QueryAsync<Produto>(
@@ -30,17 +32,18 @@ namespace MauiAppMinhasCompras.Helpers
             );
         }
 
-        public Task<int> Delete(int id)
+        public Task<int> Delete(int id) //Método Delete remove um registro da tabela Produto com base no Id fornecido (assíncrono)
         {
             return _conn.Table<Produto>().DeleteAsync(i => i.Id == id);
         }
 
-        public Task<List<Produto>> GetAll()
+        public Task<List<Produto>> GetAll() //Consulta a tabela Produto no BDs e retorna os registros em lista de objetos Produto (assíncrono)
         {
             return _conn.Table<Produto>().ToListAsync();
         }
 
-        public Task<List<Produto>> Search(string q)
+        public Task<List<Produto>> Search(string q) //Método Search implementa uma funcionalidade de busca na tabela Produto do BDS SQLite,
+                                                    //pode filtrar registros com uma string fornecida
         {
             string sql = "SELECT * Produto WHERE descricao LIKE '%" + q + "%'";
             return _conn.QueryAsync<Produto>(sql);
